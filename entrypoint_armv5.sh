c6943b720ea44dbfcf67c19d22e4716edb69d498
Peter Solomon <122653016+Medium1992@users.noreply.github.com>
2026-01-20 23:49:50 +0300

    Update entrypoint_armv5.sh

--- entrypoint_armv5.sh
+++ entrypoint_armv5.sh
@@ -20,26 +20,6 @@ else
   USE_NFT=false
 fi
 
-if [ "$USE_NFT" = "false" ]; then
-  if ! apk info -e iptables iptables-legacy >/dev/null; then
-    echo "Install iptables"
-    apk add --no-cache iptables iptables-legacy >/dev/null 2>&1
-    rm -f /usr/sbin/iptables /usr/sbin/iptables-save /usr/sbin/iptables-restore
-    ln -s /usr/sbin/iptables-legacy /usr/sbin/iptables
-    ln -s /usr/sbin/iptables-legacy-save /usr/sbin/iptables-save
-    ln -s /usr/sbin/iptables-legacy-restore /usr/sbin/iptables-restore
-  fi
-else
-  if ! apk info -e nftables >/dev/null; then
-    echo "Install nftables"
-    apk add --no-cache nftables >/dev/null 2>&1
-  fi
-  if apk info -e iptables iptables-legacy >/dev/null; then
-    echo "Delete iptables"
-    apk del iptables iptables-legacy >/dev/null 2>&1
-  fi
-fi
-
 log() { echo "[$(date +'%H:%M:%S')] $*"; }
 
 first_iface() {
